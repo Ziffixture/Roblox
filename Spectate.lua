fb14c84f1388b6db6fa03e79e9d3dcca073dd78c
Ziffix <125783558+Ziffixture@users.noreply.github.com>
2024-10-12 06:45:37 -0600

    Create Spectate.lua

---
+++ Spectate.lua
@@ -0,0 +1,225 @@
+--[[
+Author     Ziffixture (74087102)
+Date       24/10/12 (YY/MM/DD)
+Version    1.0.0
+]]
+
+
+
+--!strict
+local ContextActionService = game:GetService("ContextActionService")
+local ReplicatedStorage    = game:GetService("ReplicatedStorage")
+local UserInputService     = game:GetService("UserInputService")
+local StarterGui           = game:GetService("StarterGui")
+local Players              = game:GetService("Players")
+
+
+local PLAYER_MOVEMENT_DISABLE_FLAG = "DisablePlayerMovement"
+
+local CURRENT_CAMERA = workspace.CurrentCamera
+local LOCAL_PLAYER   = Players.LocalPlayer
+
+
+local Vendor  = ReplicatedStorage:WaitForChild("Vendor")
+local Connect = require(Vendor:WaitForChild("Connect"))
+local Signal  = require(Vendor:WaitForChild("Signal"))
+
+local Feature = script.Parent
+local Types   = require(Feature:WaitForChild("Types"))
+
+local Gui       = LOCAL_PLAYER.PlayerGui:WaitForChild("Spectate")
+local Spectate  = Gui:WaitForChild("Spectate")
+local Container = Gui:WaitForChild("Container")
+
+local Centre   = Container:WaitForChild("Centre")
+local Username = Centre:WaitForChild("Username")
+local Previous = Centre:WaitForChild("Previous")
+local Next     = Centre:WaitForChild("Next")
+
+
+local tray: Types.Tray = {
+	KeyBindConnections = {},
+	
+	RawCharacterAddedConnections    = {},
+	RawCharacterRemovingConnections = {},
+	
+	ButtonConnections = {},
+}
+
+local isSpectating = false
+
+
+
+local function disablePlayerMovement()
+	ContextActionService:BindAction(
+		PLAYER_MOVEMENT_DISABLE_FLAG,
+		function()
+			return Enum.ContextActionResult.Sink
+		end,
+		false,
+		table.unpack(Enum.PlayerActions:GetEnumItems())
+	)
+end
+
+local function enablePlayerMovement()
+	ContextActionService:UnbindAction(PLAYER_MOVEMENT_DISABLE_FLAG)
+end
+
+local function toggleReset(canReset: boolean)
+	pcall(function()
+		StarterGui:SetCore("ResetButtonCallback", canReset)
+	end)
+end
+
+local function safeInput(keycode: Enum.KeyCode, callback: (InputObject) -> ())
+	return UserInputService.InputBegan:Connect(function(input: InputObject, gameProcessedEvent: boolean)
+		if gameProcessedEvent then
+			return
+		end
+		
+		if input.KeyCode ~= keycode then
+			return
+		end
+		
+		callback(input)
+	end)
+end
+
+local function getTrackedCharacters(excludePlayers: {Player}): ({Types.Character}, Signal.Signal<>, Signal.Signal<>)
+	local characters = {}
+	
+	local characterAdded   = Signal.new()
+	local characterRemoved = Signal.new()
+	
+	local function removeFromCharacters(character: Model, player: Player)
+		local index = table.find(characters, character :: Types.Character)
+		if not index then
+			return
+		end
+
+		table.remove(characters, index)
+
+		characterRemoved:Fire()
+	end
+
+	local function addToCharacters(character: Model, player: Player)
+		table.insert(characters, character :: Types.Character)
+		
+		characterAdded:Fire()
+	end
+
+	for _, player in Players:GetPlayers() do
+		if table.find(excludePlayers, player) then
+			continue
+		end
+
+		local characterAdded, characterRemoving = Connect.character(player, addToCharacters, removeFromCharacters)
+
+		table.insert(tray.RawCharacterAddedConnections, characterAdded :: RBXScriptConnection)
+		table.insert(tray.RawCharacterRemovingConnections, characterRemoving :: RBXScriptConnection)
+	end
+	
+	return characters, characterAdded, characterRemoved
+end
+
+local function setCameraToCharacter(character: Types.Character?)
+	if not character then
+		return
+	end
+	
+	local humanoid = character:FindFirstChildOfClass("Humanoid")
+	if humanoid then
+		CURRENT_CAMERA.CameraSubject = humanoid
+	end
+end
+
+local function stopSpectating()
+	Container.Visible = false
+	isSpectating      = false
+	
+	setCameraToCharacter(LOCAL_PLAYER.Character)
+	enablePlayerMovement()
+	toggleReset(true)
+
+	Connect.cleanKeys(tray.KeyBindConnections)
+
+	Connect.clean(tray.RawCharacterAddedConnections)
+	Connect.clean(tray.RawCharacterRemovingConnections)
+
+	Connect.clean(tray.ButtonConnections)
+
+	;(tray.CharacterRemoved :: Signal.Connection):Disconnect()
+end
+
+local function startSpectating()
+	local characters, _, characterRemoved = getTrackedCharacters({LOCAL_PLAYER})
+	if #characters == 0 then
+		return
+	end
+	
+	local index = 1
+	
+	Container.Visible = true
+	isSpectating      = true
+
+	disablePlayerMovement()
+	toggleReset(false)
+	
+	local function setCharacter()
+		if #characters == 0 then
+			stopSpectating()
+			
+			return
+		end
+		
+		local character = characters[index]
+		setCameraToCharacter(character)
+
+		Username.Text = character.Name
+	end
+	
+	local function offsetIndex(offset)
+		local length = #characters
+		
+		index = (index + offset - 1) % length + 1
+		if index < 1 then
+			index += length
+		end
+	end
+	
+	local function nextCharacter()
+		offsetIndex(1)
+		setCharacter()
+	end
+	
+	local function previousCharacter()
+		offsetIndex(-1)
+		setCharacter()
+	end
+
+	tray.CharacterRemoved     = characterRemoved:Connect(nextCharacter)
+	tray.KeyBindConnections.E = safeInput(Enum.KeyCode.E, nextCharacter)
+	tray.KeyBindConnections.Q = safeInput(Enum.KeyCode.Q, previousCharacter)
+	
+	local nextConnection     = Next.Activated:Connect(nextCharacter)
+	local previousConnection = Previous.Activated:Connect(previousCharacter)
+	
+	table.insert(tray.ButtonConnections, nextConnection)
+	table.insert(tray.ButtonConnections, previousConnection)
+	
+	setCharacter()
+end
+
+local function onSpectate()
+	if isSpectating then
+		stopSpectating()
+	else
+		startSpectating()
+	end
+end
+
+
+
+toggleReset(true)
+
+Spectate.Activated:Connect(onSpectate)
